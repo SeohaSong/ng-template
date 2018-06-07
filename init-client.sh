17acc75a1e15b2d79384290ac60d6e2d4aff491a
seohasong <tisutoo@gmail.com>
2018-06-07 10:31:45 +0900

    update origin master

--- init-client.sh
+++ init-client.sh
@@ -1,44 +1,49 @@
+(
+    if [ "$1" != "" ]
+    then
 
-if [ "$1" != "" ]
+    ng new --style scss $1
+    cd $1
+    ng g universal --app-id $1 --client-project $1
+    npm install --save\
+    ts-loader\
+    @nguniversal/module-map-ngfactory-loader\
+    @nguniversal/express-engine\
+    @nguniversal/common\
+    webpack-cli
+
+    cd ..
+
+    cat script/app.server.module.ts > $1/src/app/app.server.module.ts 
+    cat script/tsconfig.server.json > $1/src/tsconfig.server.json
+    sed "s/APP_NAME/$1/g" script/server.ts > $1/server.ts
+    sed "s/APP_NAME/$1/g" script/prerender.ts > $1/prerender.ts
+    cat script/webpack.server.config.js > $1/webpack.server.config.js
+    sed "s/APP_NAME/$1/g" script/package.json > $1/package.json
+    cat script/static.paths.ts > $1/static.paths.ts
+
+    rm -rf .git README.md script
+    cd ..
+    mv template.client $1'.client'
+    cd $1'.client'
+
+    echo \
+    "
+    cd $1.client\\
+    && mv dist dist_\\
+    && npm run build:prerender\\
+    && mv dist_/$1/.git dist/$1\\
+    && rm -rf dist_\\
+    && cd dist/$1\\
+    && (git add .; git commit -m 'update origin master'; git push origin master)
+    " > build-client.sh
+
+    rm -rf init-client.sh
+
+    fi
+)
+
+if [ -d "$1" ]
 then
-
-ng new --style scss $1
-cd $1
-ng g universal --app-id $1 --client-project $1
-npm install --save\
- ts-loader\
- @nguniversal/module-map-ngfactory-loader\
- @nguniversal/express-engine\
- @nguniversal/common\
- webpack-cli
-
-cd ..
-
-cat client-template/app.server.module.ts > $1/src/app/app.server.module.ts 
-cat client-template/tsconfig.server.json > $1/src/tsconfig.server.json
-sed "s/APP_NAME/$1/g" client-template/server.ts > $1/server.ts
-sed "s/APP_NAME/$1/g" client-template/prerender.ts > $1/prerender.ts
-cat client-template/webpack.server.config.js > $1/webpack.server.config.js
-sed "s/APP_NAME/$1/g" client-template/package.json > $1/package.json
-cat client-template/static.paths.ts > $1/static.paths.ts
-
-rm -rf .git README.md client-template
-cd ..
-mv web-template $1
-cd $1
-mv $1 $1'.client'
-
-echo \
-"
-cd $1.client\\
-&& mv dist dist_\\
-&& npm run build:prerender\\
-&& mv dist_/$1/.git dist/$1\\
-&& rm -rf dist_\\
-&& cd dist/$1\\
-&& (git add .; git commit -m 'update origin master'; git push origin master)
-" > build-client.sh
-
-rm -rf init-client.sh
-
+    cd .
 fi
